85799f5c33df66690796d8c8c7fd91b3b66a2662
oldman07 <backagain010@gmail.com>
2023-03-02 12:01:57 +0500

    shop page setup

--- laravel/resources/views/shop/shop.blade.php
+++ laravel/resources/views/shop/shop.blade.php
@@ -5,15 +5,47 @@
 <h1>All Products</h1>
 
 @foreach ($product  as $product)
+
 <div id="section2" class="section2">
-<div class="container">
+    <div class="container" >
+        <div class="items">
+          <div class="img img1"><button class="button" id="Ok"><a href= "{{ url('/individual') }}/{{ $product->product_id }}"><img src="https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""></a></div></button>
+          <div class="name">Product Name: {{ $product->product_name }}</div>
+          <div class="price">Product Category: {{ $product->category }}</div>
+          {{-- <div class="info">Lorem ipsum dolor sit amet consectetur.</div> --}}
+        </div>
+     </div>
+    </div>
+    <div id="section2" class="section2">
+        <div class="container" >
+            <div class="items">
+              <div  class="img img1"><button class="button" id="Ok"><a href= "{{ url('/individual') }}/{{ $product->product_id }}"><img src="https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""></a></div></button>
+              <div class="name">Product Name: {{ $product->product_name }}</div>
+              <div class="price">Product Category: {{ $product->category }}</div>
+              {{-- <div class="info">Lorem ipsum dolor sit amet consectetur.</div> --}}
+            </div>
+         </div>
+        </div>
+        <div id="section2" class="section2">
+            <div class="container" >
+                <div class="items">
+                  <div class="img img1"><button class="button" id="Ok"><a href= "{{ url('/individual') }}/{{ $product->product_id }}"><img src="https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""></a></div></button>
+                  <div class="name">Product Name: {{ $product->product_name }}</div>
+                  <div class="price">Product Category: {{ $product->category }}</div>
+                  {{-- <div class="info">Lorem ipsum dolor sit amet consectetur.</div> --}}
+                </div>
+             </div>
+            </div>
+{{-- <div id="section2" class="section2">
+<div class="container" >
     <div class="items">
       <div class="img img1"><button class="button" id="Ok"><a href= "{{ url('/individual') }}/{{ $product->product_id }}"><img src="https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""></a></div></button>
       <div class="name">Product Name: {{ $product->product_name }}</div>
       <div class="price">Product Category: {{ $product->category }}</div>
       {{-- <div class="info">Lorem ipsum dolor sit amet consectetur.</div> --}}
-    </div>
-</div>
-</div>
+    {{-- </div>
+ </div>
+</div> --}}
 @endforeach
 @endsection
+
